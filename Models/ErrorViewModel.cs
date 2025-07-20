cfdacdbe76b0131d3fa51ffe7dd594b91bee9483
khushwant18 <desaledevesh42@gmail.com>
2025-07-20 18:46:30 +0530

    Upload Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Firsst_pipeline.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
